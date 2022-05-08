bdfb52a201a66afb6e08691f3596351bb242c182
vaska <vaskania26@gmail.com>
2022-05-08 20:29:08 +0400

    github finder

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import spinner from './assets/spinner.gif';
+
+const Spinner = () => {
+  return (
+      <div className='w-100 mt-20'>
+        <img className='text-center mx-auto' width={180} src={spinner} alt='Loading...'/>
+      </div>
+  );
+};
+
+export default Spinner;
